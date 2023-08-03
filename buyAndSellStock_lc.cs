6894092f03f331771380e03256e041f20ec688a4
Prashanth Sridhar <sridharprashanth27@gmail.com>
2023-08-03 20:25:30 +0530

    Create buyAndSellStock_lc.cs

---
+++ buyAndSellStock_lc.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        if (prices.Length == 1)
+            return 0;
+        int maxDif = prices[1] - prices[0];
+        int min = prices[0];
+        for (int i = 1; i < prices.Length; i++)
+        {
+            if (prices[i] - min > maxDif)
+            {
+                maxDif = prices[i] - min;
+            }
+            if (prices[i] < min)
+                min = prices[i];
+        }
+        if (maxDif < 0)
+            maxDif = 0;
+        return maxDif;
+    }
+}
