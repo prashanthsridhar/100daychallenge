33d159f5db81ffebcd2431c5909043a114bba866
Prashanth Sridhar <sridharprashanth27@gmail.com>
2023-08-23 17:49:55 +0530

    Create ReverseWords.cs

---
+++ ReverseWords.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public string ReverseWords(string s) {
+        String[] strlist = s.Split(" ",
+               StringSplitOptions.RemoveEmptyEntries);
+
+            string temp = "";
+            foreach (String ss in strlist)
+            {
+                temp = ss.Trim() +" "+ temp;
+            }
+
+            return temp.Substring(0,temp.Length-1);
+        
+    }
+}
